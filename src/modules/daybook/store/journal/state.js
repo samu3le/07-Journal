ca7456d91e5718e8d8204df57008e846d56c50d7
Samuel Espinoza <samuelesponline@gmail.com>
2021-10-28 22:54:48 -0500

    Punto de restauracionl

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,22 @@
 export default () => ({
     isLoading: true,
-    entries: [1, 2, 3, 4, 5, 6],
+    entries: [{
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            Text: "Occaecat consequat sit dolore consequat esse voluptate laborum et. Cupidatat non sit cillum sint incididunt consectetur minim. Enim voluptate anim ex do minim Lorem dolore qui veniam cupidatat. Nisi sint nostrud laborum dolor duis voluptate reprehenderit. Veniam irure ex enim ea. Ipsum elit laborum id excepteur amet sunt ut incididunt non. Ut ipsum eu veniam irure ex enim commodo est laboris. Lorem consequat cillum nisi esse cupidatat sunt sint deserunt laborum qui",
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            Text: "Sint eiusmod do cillum fugiat anim ullamco eu dolore veniam sunt proident dolor adipisicing. Anim duis nisi velit sint. Ullamco esse officia incididunt aliquip consequat aliqua exercitation est est sint non. Laboris eiusmod ipsum fugiat tempor ullamco ea fugiat aliqua proident irure consectetur. In enim eiusmod ipsum ea excepteur esse esse proident occaecat id deserunt non nisi dolore. Esse fugiat officia fugiat adipisicing nulla labore. Dolor reprehenderit in consectetur est cupidatat excepteur eiusmod commodo fugiat eu consequat aute eiusmod Veniam eu exercitation ea aute in ex nisi. Cillum sunt sunt ipsum voluptate aliquip. Proident qui proident ea laborum mollit.",
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            Text: "Enim amet amet cupidatat exercitation ipsum qui amet minim eu veniam laboris adipisicing. Eiusmod eu duis nostrud non ea culpa. Elit incididunt labore esse aute enim dolore. Reprehenderit eiusmod tempor consectetur exercitation. Mollit dolor exercitation ad exercitation in do et enim do id et labore nostrud. Dolor incididunt labore amet magna consectetur sint enim. Esse ullamco nulla est laborum ut do adipisicing. Deserunt ea dolore aliqua incididunt velit qui ipsum irure id anim voluptate nisi minim anim. Est mollit ipsum ullamco dolor est reprehenderit excepteur quis. Aliqua est fugiat ipsum enim aliqua sunt voluptate ut et non. Elit nisi ullamco elit non. Ullamco adipisicing ipsum Lorem officia non occaecat culpa ad minim.",
+            picture: null,
+        },
+    ],
 });
